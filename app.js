a2d49ef18c8a57a01eb720c44492d79ca5e96b1d
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-02-08 21:31:30 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var app = express();
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
-var PORT = process.env.PORT || 5000
+var PORT = process.env.PORT || 3000
 var server = app.listen(PORT,function () {
 
   var host = server.address().address
   var port=PORT
 
   console.log("Example app listening at http://%s:%s", host, port)
-    console.log(process.env.PORT)
+
 })
