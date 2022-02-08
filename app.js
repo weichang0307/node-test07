4a1a4f68a974209c57e201344bbec627b58c8eb3
weichang0307 <83111522+weichang0307@users.noreply.github.com>
2022-02-08 19:34:43 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const { response } = require("express");
+const http =require("http");
+const server =http.createServer((request,response)=>{
+    response.end("hello")
+
+})
+const port=3000
+const ip="127.0.0.1"
+server.listen(port,ip,()=>{
+    console.log('start')
+})
